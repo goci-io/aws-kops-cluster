4e01e982b89811ed2ed3b1fc2fa3d3b2ef5a5cf5
etwillbefine <tim.gatzemeier@gmx.de>
2019-09-14 23:35:43 +0200

    use -spot and non spot ig template

--- instance-groups.tf
+++ instance-groups.tf
@@ -1,8 +1,12 @@
+locals {
+  optional_max_price_format = "\n  maxPrice: \"%d\"\n"
+}
+
 data "null_data_source" "instance_groups" {
   count = length(var.instance_groups) * var.max_availability_zones
 
   inputs = {
-    rendered = templatefile("${path.module}/templates/instance-group.yaml", {
+    rendered = templatefile("${path.module}/templates/instance-group${lookup(var.instance_groups[floor(count.index / 3)], "autospotting", false) ? "-spot" : ""}.yaml", {
       cluster_name           = local.cluster_name
       namespace              = var.namespace
       stage                  = var.stage
@@ -13,13 +17,13 @@ data "null_data_source" "instance_groups" {
       image                  = lookup(var.instance_groups[floor(count.index / 3)], "image", local.kops_default_image)
       instance_name          = lookup(var.instance_groups[floor(count.index / 3)], "name")
       instance_type          = lookup(var.instance_groups[floor(count.index / 3)], "instance_type")
-      instance_max           = lookup(var.instance_groups[floor(count.index / 3)], "count_max", 3)
+      instance_max           = lookup(var.instance_groups[floor(count.index / 3)], "count_max", 5)
       instance_min           = lookup(var.instance_groups[floor(count.index / 3)], "count_min", 1)
       storage_type           = lookup(var.instance_groups[floor(count.index / 3)], "storage_type", "gp2")
       storage_iops           = lookup(var.instance_groups[floor(count.index / 3)], "storage_iops", 168)
       storage_in_gb          = lookup(var.instance_groups[floor(count.index / 3)], "storage_in_gb", 56)
-      autospotting_enabled   = lookup(var.instance_groups[floor(count.index / 3)], "autospotting", false)
-      autospotting_max_price = lookup(var.instance_groups[floor(count.index / 3)], "autospotting", false) ? format("maxPrice: \"%d\"", lookup(var.instance_groups[floor(count.index / 3)], "autospotting_max_price", "0.03")) : ""
+      autospotting_instances = join("\n    - %s", lookup(var.instance_groups[floor(count.index / 3)], "autospotting_instances", [lookup(var.instance_groups[floor(count.index / 3)], "instance_type")]))
+      autospotting_max_price = lookup(var.instance_groups[floor(count.index / 3)], "autospotting", false) ? format(local.optional_max_price_format, lookup(var.instance_groups[floor(count.index / 3)], "autospotting_max_price", 0.03)) : ""
 
       instance_group_name = format(
         "%s-%s", 
@@ -53,8 +57,6 @@ data "null_data_source" "master_instance_groups" {
       storage_type           = "io1"
       storage_iops           = 480
       storage_in_gb          = 156
-      autospotting_enabled   = false
-      autospotting_max_price = ""
       node_role              = "Master"
       instance_name          = "master"
       instance_type          = var.master_machine_type
@@ -66,7 +68,7 @@ data "null_data_source" "master_instance_groups" {
 
 data "null_data_source" "bastion_instance_group" {
   inputs = {
-    rendered = templatefile("${path.module}/templates/instance-group.yaml", {
+    rendered = templatefile("${path.module}/templates/instance-group-spot.yaml", {
       cluster_name           = local.cluster_name
       namespace              = var.namespace
       stage                  = var.stage
@@ -78,8 +80,7 @@ data "null_data_source" "bastion_instance_group" {
       storage_type           = "gp2"
       storage_iops           = 0
       storage_in_gb          = 6
-      autospotting_enabled   = true
-      autospotting_max_price = "maxPrice: \"0.005\""
+      autospotting_max_price = 0.005
       instance_group_name    = "bastion"
       node_role              = "Bastion"
       instance_name          = "bastion"
