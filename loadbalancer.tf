857eb1be1adb21a103ec47af5ebf8c29ad55ad6e
etwillbefine <tim.gatzemeier@gmx.de>
2020-01-25 17:37:55 +0100

    allow to create additional, public api load balancer if there is no one specified via remote state or variables the created one has precedence, otherwise it wont be used in auto scaling groups

---
+++ loadbalancer.tf
@@ -0,0 +1,67 @@
+
+locals {
+  # Deploy additional public loadbalancer
+  # This covers a setup where a private and public hosted zone exists and the API Server should be publicly available
+  create_additional_loadbalancer = var.create_load_balancer && var.master_ips_for_private_api_dns && !local.external_lb_enabled && var.cluster_dns_type != "Private"
+}
+
+module "api_loadbalancer_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
+  context    = module.label.context
+  attributes = ["api"]
+}
+
+resource "aws_security_group" "public_loadbalancer" {
+  count       = local.create_additional_loadbalancer ? 1 : 0
+  name        = module.api_loadbalancer_label.id
+  tags        = module.api_loadbalancer_label.tags
+  description = "Allows public HTTPS inbound traffic to API Server"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "public_api" {
+  count                      = local.create_additional_loadbalancer ? 1 : 0
+  name                       = module.api_loadbalancer_label.id
+  tags                       = module.api_loadbalancer_label.tags
+  security_groups            = aws_security_group.public_loadbalancer.*.id
+  load_balancer_type         = var.load_balancer_type
+  subnets                    = local.public_subnet_ids
+  internal                   = false
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.kops_state.id
+    prefix  = "public-api-access-logs"
+    enabled = true
+  }
+}
+
+resource "aws_lb_target_group" "api" {
+  count    = local.create_additional_loadbalancer ? 1 : 0
+  name     = module.api_loadbalancer_label.id
+  tags     = module.api_loadbalancer_label.tags
+  vpc_id   = local.vpc_id
+  protocol = "HTTPS"
+  port     = 443
+}
+
+resource "aws_lb_listener" "api" {
+  count             = local.create_additional_loadbalancer ? 1 : 0
+  load_balancer_arn = join("", aws_lb.public_api.*.arn)
+  certificate_arn   = local.certificate_arn
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  protocol          = "HTTPS"
+  port              = 443
+
+  default_action {
+    type             = "forward"
+    target_group_arn = join("", aws_lb_target_group.api.*.arn)
+  }
+}
