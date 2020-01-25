12a115576fe938a1212e233c3924870369e869a2
etwillbefine <tim.gatzemeier@gmx.de>
2020-01-25 19:34:53 +0100

    add https egress to loadbalancer sg

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -31,6 +31,13 @@ resource "aws_security_group" "public_loadbalancer" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_lb" "public_api" {
