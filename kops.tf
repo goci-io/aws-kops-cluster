5a580782d0c935ff2d7fbe2742a4a4552a9f3f88
etwillbefine <tim.gatzemeier@gmx.de>
2020-02-29 16:45:38 +0100

    use newer kops image as base ami trying to avoid issues with calcio which indicates a duplicate cni config therefore updating is anyway a good idea.

--- kops.tf
+++ kops.tf
@@ -55,7 +55,7 @@ locals {
     additional_master_policies = var.additional_master_policies == "" ? "" : indent(6, replace(var.additional_master_policies, "/\\\"/", "\\\""))
   })
 
-  kops_default_image = "kope.io/k8s-1.15-debian-stretch-amd64-hvm-ebs-2019-09-26"
+  kops_default_image = "kope.io/k8s-1.15-debian-stretch-amd64-hvm-ebs-2020-01-17"
   kops_configs = concat(
     [data.null_data_source.bastion_instance_group.outputs],
     data.null_data_source.master_instance_groups.*.outputs,
