5bb9d0d9dcac6493f9df96a99f559e3e7c9a5edb
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-21 20:05:05 +0200

    use version 0.3.0 for route53-records module

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -43,7 +43,7 @@ resource "aws_elb" "public_api" {
 }
 
 module "public_api_record" {
-  source        = "git::https://github.com/goci-io/aws-route53-records.git?ref=master"
+  source        = "git::https://github.com/goci-io/aws-route53-records.git?ref=tags/0.3.0"
   enabled       = var.create_public_api_record
   hosted_zone   = local.cluster_dns
   alias_records = [
