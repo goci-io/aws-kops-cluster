c5fc1d4bfef443119201fe15c80409da4e1164cf
Tim Gatzemeier <etwillbefine@users.noreply.github.com>
2020-05-12 11:38:57 +0200

    use valid supported etcd version

--- variables.tf
+++ variables.tf
@@ -59,7 +59,7 @@ variable "masters_instance_count" {
 
 variable "etcd_version" {
   type        = string
-  default     = "3.4.7"
+  default     = "3.4.3"
   description = "Version of etcd to use for kubernetes backend"
 }
 
